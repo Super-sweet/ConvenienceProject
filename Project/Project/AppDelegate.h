f60ebea3c12f07b6479c4062e13726e97cc712f0
xuchao <347393193@qq.com>
2018-09-12 10:07:27 +0800

    初始化项目，创建工程

---
+++ Project/Project/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Project
+//
+//  Created by xuchao on 2018/9/12.
+//  Copyright © 2018 xuchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Project/Project/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Project
+//
+//  Created by xuchao on 2018/9/12.
+//  Copyright © 2018 xuchao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
